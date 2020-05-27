2d7803b68942aa65498702e5c01eb47e51b9a6b2
adam2809 <adam.kule@gmail.com>
2020-05-27 20:31:41 +0200

    Error checking for argument input and set up interval

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,17 @@
-fun main(args:Array<String>) {
-    val argsMap = args.toList().windowed(2).map { (a,b) -> a to b }
-    println(argsMap)
+import kotlin.concurrent.timer
+import kotlin.system.exitProcess
+
+fun main(args:Array<String>){
+    val argsMap:Map<String,String> = args.toList().windowed(2).map { (a,b) -> a to b }.toMap()
+    val interval:Long
+    try {
+        interval = argsMap["-i"]?.toLong() ?: 10
+    }catch(e:NumberFormatException){
+        println("The interval value is not in the correct format. Please use a number format.")
+        exitProcess(1)
+    }
+
+    timer(period=interval*1000){
+        println("szekszekszek")
+    }
 }
