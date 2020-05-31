df7f28e8c6b38db3b98fbbb1a4892811dce9d869
adam2809 <adam.kule@gmail.com>
2020-05-31 15:03:59 +0200

    Created template for alert testing class

--- src/test/kotlin/AlertTest.kt
+++ src/test/kotlin/AlertTest.kt
@@ -1,10 +1,34 @@
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+import java.io.ByteArrayOutputStream
+import java.io.FileDescriptor
+import java.io.FileOutputStream
+import java.io.PrintStream
+
 
 class AlertTest{
 
+    private val stdOut = ByteArrayOutputStream()
+
+    @BeforeAll
+    fun redirectStdOut(){
+        System.setOut(PrintStream(stdOut))
+    }
+
+    @AfterAll
+    fun restoreStdOut(){
+        System.setOut(PrintStream(FileOutputStream(FileDescriptor.out)));
+    }
+
     @Test
-    fun alertTest(){
-        assertEquals("","")
+    fun `check if user is alerted about a website going down`(){
+        fail("Not implemented")
+    }
+
+    @Test
+    fun `check if user is alerted about a website going back up`(){
+        fail("Not implemented")
     }
 }
